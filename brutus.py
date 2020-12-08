dd23c8c32cf4071fe560d4029ad823ae123e51dc
Michael Lan <michaellan202@gmail.com>
2020-12-07 17:04:36 -0800

    output collection

--- brutus.py
+++ brutus.py
@@ -12,7 +12,8 @@ import sys
 import argparse
 import pathlib
 import io
-from typing import IO, List
+import shlex
+from typing import List, Iterator
 
 def get_args():
     parser = argparse.ArgumentParser(description='brutus')
@@ -20,24 +21,50 @@ def get_args():
     group.add_argument('-g', type=argparse.FileType('r'), metavar='GENFILE', default='gen.py')
     group.add_argument('-i', type=argparse.FileType('r'), metavar='INPUTFILE', default='input')
     parser.add_argument('-o', type=pathlib.Path, metavar='OUTPUTFILE', default='output')
-    parser.add_argument('-s', type=argparse.FileType('r'), metavar='SRCFILE')
-    parser.add_argument('-d', type=argparse.FileType('r'), metavar='DELIMITER', default='===')
+    parser.add_argument('-s', type=argparse.FileType('r'), metavar='RUNCMDFILE', default='runcmd')
+    parser.add_argument('-d', type=str, metavar='DELIMITER', default='===')
     args = parser.parse_args()
     return args
 
-def run_single_testcase(stream: IO, cmd: List[str]) -> str:
-    return subprocess.run(cmd, stdin=stream, stdout=subprocess.PIPE).stdout.decode('utf-8')
+def run_single_testcase(data: str, cmd: List[str]) -> str:
+    return subprocess.run(cmd, input=data.encode('utf-8'), stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-def gen_input(genfile: pathlib.Path, args: argparse.Namespace, num_tests=3) -> str:
+def gen_input(genfile: pathlib.Path, delim: str, num_tests=3) -> str:
     res = ''
     for _ in range(num_tests):
         res += subprocess.run(['python', genfile.name], stdout=subprocess.PIPE).stdout.decode('utf-8')
         if not res.endswith('\n'):
             res += '\n'
-        res += f'{args.d}\n'
+        res += f'{delim}\n'
     return res
 
+def gen_testcases(lines: str, delim: str) -> Iterator[str]:
+    res = []
+    test_case_token = ''
+    for line in lines.split('\n'):
+        if line == delim:
+            data = test_case_token
+            yield data
+            test_case_token = ''
+        else:
+            test_case_token += f'{line}\n'
+
 def main(args: argparse.Namespace):
+
+    GENFILE = args.g
+    INPUTFILE = args.i
+    OUTPUTFILE = args.o
+    RUNCMDFILE = args.s
+    DELIMITER = args.d
+
+    #
+    # get run command
+    #
+
+    # run_cmd = shlex.split(RUNCMDFILE.read().strip())
+
+    run_cmds = map(shlex.split, RUNCMDFILE.readlines())
+    print(run_cmds)
 
     #
     # get input data
@@ -45,10 +72,10 @@ def main(args: argparse.Namespace):
 
     test_input = "Nothing to see here."
 
-    if args.g is not None:
-        test_input = gen_input(args.g, args.d)
-    elif args.i is not None:
-        test_input = args.i.read()
+    if GENFILE is not None:
+        test_input = gen_input(GENFILE, DELIMITER)
+    elif INPUTFILE is not None:
+        test_input = INPUTFILE.read()
     else:
         sys.exit('no method of input given')
 
@@ -56,10 +83,17 @@ def main(args: argparse.Namespace):
     # generate output data
     #
 
-    test_case_token = ''
-    for line in test_input.split('\n'):
-        if line == args.d:
+    with open(OUTPUTFILE, 'w+') as outputfile:
+        for testcase in gen_testcases(test_input, DELIMITER):
+            outputfile.write(f'{testcase}:\n')
 
+            for rc in run_cmds:
+                outputfile.write(f'{rc}:\n')
+                output = run_single_testcase(testcase, rc)
+                if not output.endswith('\n'):
+                    output += '\n'
+                outputfile.write(output)
+                outputfile.write(f'{DELIMITER}\n')
 
 
 if __name__ == '__main__':
